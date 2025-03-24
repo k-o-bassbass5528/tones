6fc429495bd2b100ba6e406c4612b979b7a1eaab
k-o-bassbass5528 <k.o.bassbass@gmail.com>
2025-03-24 23:52:46 +0900

    [add]バリデーション機能

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,10 @@ class Post < ApplicationRecord
     has_many :comments, dependent: :destroy
     has_many :favorites, dependent: :destroy
 
+    validates :instrument, presence: true, length: { maximum: 50 }
+    validates :text, presence: true, length: { maximum: 250 }
+    validates :image, presence: true
+
     def favorited_by?(user)
         favorites.where(user_id: user.id).exists?
     end
