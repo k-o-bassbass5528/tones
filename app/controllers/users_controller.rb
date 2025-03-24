c0be4d26eab376bc9482ed36d3aa70ef0d0eb2a6
k-o-bassbass5528 <k.o.bassbass@gmail.com>
2025-03-24 22:25:11 +0900

    [add]フォロー機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
 
   def followers
     user = User.find(params[:id])
-    @users = user.follwer_user.page(params[:page]).per(3).reverse_order
+    @users = user.follower_user.page(params[:page]).per(3).reverse_order
   end
 
   private
