2d2124e55512caed76e944ab4dd04119b9573f1e
k-o-bassbass5528 <k.o.bassbass@gmail.com>
2025-04-02 20:57:10 +0900

    文字数等微修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :post
 
-    validates :comment, presence: true, length: { maximum: 130 }
+    validates :comment, presence: true, length: { maximum: 120 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ApplicationRecord
     has_many :favorites, dependent: :destroy
 
     validates :instrument, presence: true, length: { maximum: 50 }
-    validates :text, presence: true, length: { maximum: 240 }
+    validates :text, presence: true, length: { maximum: 200 }
     validates :image, presence: true
 
     enum status: { published: 0, draft: 1 }
