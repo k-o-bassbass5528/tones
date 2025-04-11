6c0f9f4e2d42de64eb6ee247fcda4811399e0f86
k-o-bassbass5528 <k.o.bassbass@gmail.com>
2025-04-11 22:05:49 +0900

    リダイレクト先修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -35,7 +35,7 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
     if @post.update(post_params)
-      redirect_to post_path(post.id)
+      redirect_to post_path(@post.id)
     else
       render :edit, status: :unprocessable_entity
     end
