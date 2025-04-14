303dd3be9c5bc3b1be13dab0be2fb41fc2a36c6c
k-o-bassbass5528 <k.o.bassbass@gmail.com>
2025-04-14 21:11:09 +0900

    カテゴリ検索機能修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,3 +4,12 @@
 //= require bootstrap
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener('turbo:load', function() {
+    const categorySelect = document.querySelector('.category-select');
+    if (categorySelect) {
+        categorySelect.addEventListener('change', function() {
+            document.getElementById('category_search_form').submit();
+        });
+    }
+});
