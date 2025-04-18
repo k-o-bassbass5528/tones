d07f792ef753c05903f6bb8043a10d630df922de
k-o-bassbass5528 <k.o.bassbass@gmail.com>
2025-04-18 21:56:34 +0900

    エラー表示修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,6 @@
 class Post < ApplicationRecord
     has_many_attached :images
     belongs_to :user
-    belongs_to :category
     has_many :comments, dependent: :destroy
     has_many :favorites, dependent: :destroy
     has_many :view_counts, dependent: :destroy
@@ -9,6 +8,7 @@ class Post < ApplicationRecord
     validates :instrument, presence: true, length: { maximum: 60 }
     validates :text, presence: true, length: { maximum: 200 }
     validates :images, presence: true
+    belongs_to :category
     validate :validate_image_count
 
     enum status: { published: 0, draft: 1 }
