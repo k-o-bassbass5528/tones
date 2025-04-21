194ca95ac736d85d0660fade2ccb6f698bb47c64
k-o-bassbass5528 <k.o.bassbass@gmail.com>
2025-04-21 21:06:16 +0900

    [add]退会機能

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,12 @@
 class Entry < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   belongs_to :room
+
+  after_destroy :destroy_room_immediately
+
+  private
+
+  def destroy_room_immediately
+    room.destroy
+  end
 end
