e24200bb98ba157108e1bdda9634eb75a393df60
k-o-bassbass5528 <k.o.bassbass@gmail.com>
2025-05-12 21:57:47 +0900

    ヘッダーのサイドバー化（途中）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,6 @@ class ApplicationController < ActionController::Base
     before_action :authenticate_user!,except: [:top]
     before_action :configure_permitted_parameters, if: :devise_controller?
 
-    layout :select_layout
-
     protected
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
@@ -12,14 +10,5 @@ class ApplicationController < ActionController::Base
 
     def after_sign_in_path_for(resource)
         posts_path
-    end
-
-    private
-    def select_layout
-        if controller_name == "home" || devise_controller?
-            "top"
-        else
-            "application"
-        end
     end
 end
